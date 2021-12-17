43d7aaf2ff5475045e19d628fae06c3a8f897db5
Vincent Bailly <vibailly@microsoft.com>
2021-12-17 13:39:49 +0100

    place N

--- keyboards/minidox/keymaps/vincentbailly/keymap.c
+++ keyboards/minidox/keymaps/vincentbailly/keymap.c
@@ -12,8 +12,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [BASE] = LAYOUT_split_3x5_3( \
    KC_Q,         KC_W,         KC_K,         KC_R,         KC_NO,             KC_Y,      KC_U,         KC_L,         KC_COMM,         KC_P, \
-   LGUI_T(KC_J), LALT_T(KC_S), LCTL_T(KC_D), LSFT_T(KC_F), KC_G,              KC_H,      LSFT_T(KC_A), LCTL_T(KC_E), LALT_T(KC_I), LGUI_T(KC_SCLN), \
-   KC_Z,         KC_X,         KC_C,         KC_V,         KC_B,              KC_N,      KC_M,         KC_O,      KC_DOT,       KC_SLSH, \
+   LGUI_T(KC_J), LALT_T(KC_S), LCTL_T(KC_N), LSFT_T(KC_F), KC_G,              KC_H,      LSFT_T(KC_A), LCTL_T(KC_E), LALT_T(KC_I), LGUI_T(KC_SCLN), \
+   KC_Z,         KC_X,         KC_C,         KC_V,         KC_B,              KC_D,      KC_M,         KC_O,      KC_DOT,       KC_SLSH, \
                            KC_LSFT,  LT(NUMBERS, KC_T),  LT(NAV,KC_BSPC),       KC_LSFT,  LT(SYM,KC_SPC), KC_LSFT \
 ),
 [NUMBERS] = LAYOUT_split_3x5_3( \
@@ -43,7 +43,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 const uint16_t PROGMEM combo1[] = {LSFT_T(KC_F), LSFT_T(KC_A), COMBO_END};
-const uint16_t PROGMEM combo2[] = {LCTL_T(KC_D), LCTL_T(KC_E), COMBO_END};
+const uint16_t PROGMEM combo2[] = {LCTL_T(KC_N), LCTL_T(KC_E), COMBO_END};
 combo_t key_combos[COMBO_COUNT] = {
     COMBO(combo1, KC_ENT),
     COMBO(combo2, KC_ESC),
