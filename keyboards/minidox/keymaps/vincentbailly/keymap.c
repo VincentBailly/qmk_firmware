4840f2e23640be5abf869666b7940a0108b2b474
Vincent Bailly <vibailly@microsoft.com>
2022-01-19 11:44:09 +0100

    less intrusive combos

--- keyboards/minidox/keymaps/vincentbailly/keymap.c
+++ keyboards/minidox/keymaps/vincentbailly/keymap.c
@@ -42,8 +42,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 };
 
-const uint16_t PROGMEM combo1[] = {LSFT_T(KC_D), LSFT_T(KC_A), COMBO_END};
-const uint16_t PROGMEM combo2[] = {LCTL_T(KC_N), LCTL_T(KC_E), COMBO_END};
+const uint16_t PROGMEM combo1[] = { LSFT_T(KC_A), LCTL_T(KC_E),   LALT_T(KC_I) COMBO_END};
+const uint16_t PROGMEM combo2[] = { LALT_T(KC_S), LCTL_T(KC_N), LSFT_T(KC_D), COMBO_END};
 const uint16_t PROGMEM combo3[] = {KC_J, KC_F, COMBO_END};
 const uint16_t PROGMEM combo4[] = {KC_U, KC_K, COMBO_END};
 
