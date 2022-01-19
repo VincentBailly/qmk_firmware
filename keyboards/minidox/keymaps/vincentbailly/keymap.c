0186e68585b9786995c4b3d69e68fb3ff1f03c09
Vincent Bailly <vibailly@microsoft.com>
2022-01-19 11:47:21 +0100

    fix syntax

--- keyboards/minidox/keymaps/vincentbailly/keymap.c
+++ keyboards/minidox/keymaps/vincentbailly/keymap.c
@@ -42,7 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 };
 
-const uint16_t PROGMEM combo1[] = { LSFT_T(KC_A), LCTL_T(KC_E),   LALT_T(KC_I) COMBO_END};
+const uint16_t PROGMEM combo1[] = { LSFT_T(KC_A), LCTL_T(KC_E), LALT_T(KC_I), COMBO_END};
 const uint16_t PROGMEM combo2[] = { LALT_T(KC_S), LCTL_T(KC_N), LSFT_T(KC_D), COMBO_END};
 const uint16_t PROGMEM combo3[] = {KC_J, KC_F, COMBO_END};
 const uint16_t PROGMEM combo4[] = {KC_U, KC_K, COMBO_END};
